a80dc560b3541466d0132ed65ceab2bae55563ae
cthacker-udel <cthacker@udel.edu>
2021-08-07 05:37:48 -0400

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+
+namespace FirstProject
+{
+    class Program
+    {
+
+        public static string MonthName(int num) {
+
+            return new DateTimeFormatInfo().GetMonthName(num);
+        
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Excercise 1
+             * 
+             */
+
+            sbyte sbyte1;
+            byte byte1;
+            short short1;
+            ushort ushort1;
+            int int1;
+            uint uint1;
+            long long1;
+            ulong ulong1;
+
+            sbyte1 = -115;
+            byte1 = 97;
+            short1 = 224;
+            ushort1 = 1190;
+            int1 = -10000;
+            uint1 = 20000;
+            long1 = 4825932;
+            ulong1 = 9707000000;
+
+            string str1 = "Hello ";
+            string str2 = "World!";
+            Console.WriteLine(concatStr(str1, str2));
+
+            Console.WriteLine(typeVerifier(str1));
+
+            Console.WriteLine(str1 ?? "left is null");
+
+            object nullableObj = null;
+            Console.WriteLine(nullableObj ?? "left is null2");
+
+            Console.WriteLine(3.ToString());
+
+
+
+
+        }
+
+        static string typeVerifier(object value) {
+
+            if (value is string)
+            {
+                return "string";
+            }
+            else if (value is int)
+            {
+                return "int";
+            }
+            else if (value is long)
+            {
+                return "long";
+            }
+            else if (value is decimal) {
+                return "decimal";
+            }
+            else
+            {
+                return "unknown";
+            }
+        
+        }
+
+
+        static string concatStr(string str1, string str2) {
+            return str1 + str2;
+        }
+
+        static string compare(double val1, double val2) {
+
+            if (Math.Abs(val1 - val2) < 0.00001)
+            {
+                return "same";
+            }
+            else {
+                return "different";
+            }
+        
+        }
+    }
+}
