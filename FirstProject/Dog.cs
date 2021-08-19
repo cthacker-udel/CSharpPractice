0bc8f8d843e9a2f32041ced09ccb0864dce475eb
cthacker-udel <cthacker@udel.edu>
2021-08-19 06:18:46 -0400

    added dog class

---
+++ FirstProject/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+    class Dog
+    {
+
+        public string name;
+        public string furColor;
+        public string breed;
+        public bool isSitting;
+        public bool isWalking;
+
+        public Dog(string newName, string newFurColor, string newBreed) {
+            this.name = newName;
+            this.furColor = newFurColor;
+            this.breed = newBreed;
+
+        }
+
+        public void bark() {
+            Console.WriteLine("WOOF");
+        }
+
+
+
+    }
+}
